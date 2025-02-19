a0577516a6f57b8d297a2617a65483e02fc21ee1
Francis Hacker Mods <francishackermods@gmail.com>
2025-02-19 14:09:16 +0800

    Create keygeneratorscript.js

---
+++ keygeneratorscript.js
@@ -0,0 +1,11 @@
+function generateKey() {
+    let chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    let keyLength = 16;
+    let key = "";
+    
+    for (let i = 0; i < keyLength; i++) {
+        key += chars.charAt(Math.floor(Math.random() * chars.length));
+    }
+    
+    document.getElementById("key").textContent = key;
+}
